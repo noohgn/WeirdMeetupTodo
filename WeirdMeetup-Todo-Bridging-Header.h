044e75eeff8a9e32190942f852dd894c2f357d47
JungHoon Lee <leejunghoon@JungHoon-Leeui-MacBook-Pro.local>
2015-03-11 22:43:00 +0900

    add parse

---
+++ WeirdMeetup-Todo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  WeirdMeetup-Todo-Bridging-Header.h
+//  WeirdMeetupTodo
+//
+//  Created by JungHoon Lee on 2015. 3. 11..
+//  Copyright (c) 2015년 양원석. All rights reserved.
+//
+
+#ifndef WeirdMeetupTodo_WeirdMeetup_Todo_Bridging_Header_h
+#define WeirdMeetupTodo_WeirdMeetup_Todo_Bridging_Header_h
+
+#import <Parse/Parse.h>
+
+#endif
